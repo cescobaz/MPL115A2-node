b27c88bf8888d85de7dd4288ba07fbdcf59f387a
Francesco Burelli <francesco.burelli@gmail.com>
2020-12-08 21:26:17 +0100

    index completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MPL115A2 } = require('./src/mpl115a2.js')
 
 const mpl115a2 = new MPL115A2()
 mpl115a2.init()
-  //.then(mpl115a2.convert.bind(mpl115a2))
+  .then(mpl115a2.convert.bind(mpl115a2))
   .then(mpl115a2.read.bind(mpl115a2))
   .then(console.log)
+  .catch(console.log)
